3cffa11abdcaa886742fad24c005a417f22824a5
Sixten <s.leemets@gmail.com>
2020-04-27 16:51:03 +0300

    registering and displaying results working

--- routes/reglist.js
+++ routes/reglist.js
@@ -2,11 +2,8 @@ var express = require("express");
 var router = express.Router();
 var db = require("../services/db");
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
-  db.getRegList((result) => {
-    res.render("regList", { title: result });
-  });
+  onGetRegList(res);
 });
 
 router.post("/", (req, res, next) => {
@@ -14,10 +11,18 @@ router.post("/", (req, res, next) => {
   let name = req.body.name;
 
   if (name) {
-    res.render("index", { title: name });
+    db.insertNewName(name, () => {
+      onGetRegList(res, true);
+    });
   } else {
-    res.render("index", { title: "test123" });
+    res.render("index", { title: "failed" });
   }
 });
+
+function onGetRegList(res, hideSubmit) {
+  db.getRegList((result) => {
+    res.render("regList", { data: JSON.stringify(result), hideSubmit: hideSubmit });
+  });
+}
 
 module.exports = router;
--- services/db.js
+++ services/db.js
@@ -1,5 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
-const uri = "mongodb+srv://rdwDbUser:rdwDbPass420@rdw-uhl2i.mongodb.net/test?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://rdwDbUser:rdwDbPass420@rdw-uhl2i.mongodb.net/test?retryWrites=true&w=majority";
 const regListCollectionName = "regList";
 const dbName = "test";
 
@@ -17,4 +18,31 @@ function getRegList(onSuccess) {
   });
 }
 
+function insertNewName(name, onSuccess) {
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((err) => {
+    if (err) throw err;
+    const collection = client.db(dbName).collection(regListCollectionName);
+    try {
+      collection.insertOne({name: name}, function (err, response) {
+        if (err) {
+          console.log("err2");
+        } else {
+          console.log(`Resp: ${response}`);
+        }
+      });
+    } catch (err) {
+      console.log("err1");
+      throw err;
+    }
+    client.close();
+    onSuccess();
+    // collection.insertOne(name, (err, result) => {
+    //   if (err) throw err;
+    //   console.log(`insertNewName inserted: \n${name}`);
+    // });
+  });
+}
+
 module.exports.getRegList = getRegList;
+module.exports.insertNewName = insertNewName;
